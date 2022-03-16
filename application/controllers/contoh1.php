1425017f0a2a13652867a10ce5b0cdfa3f78479d
Gilang-Apdillah <gilangabdilah3@gmail.com>
2022-03-16 23:56:12 +0700

    menambhkan .gitignore,application,composer.json.distirbutting.md/index.php,licensi.txt,readme.rst,system/,user_guide/

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Gilang Apdiilah
+ Saya tingga di daerah Jakarta,Condet Batu Ampar
+ olah raga yang saya sukai adalah
+ Bulutangkis";
+ }
+}
